5e1a5ac89aedef6a1a338d1686b812ad07ac5015
yu.yadong <yuyadong@zgrj.net>
2019-01-28 18:05:24 +0800

    first commit

---
+++ src/ControlButtons.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ControlButtons = ({activated, onStart, onPause, onReset, onSplit}) => {
+    // TODO: 实现ControlButtons
+    // const {activated, onStart, onPause, onReset, onSplit} = props;
+    if (activated) {
+        return (
+            <div>
+                <button onClick={onSplit}>计次</button>
+                <button onClick={onPause}>停止</button>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <button onClick={onReset}>复位</button>
+                <button onClick={onStart}>启动</button>
+            </div>
+        )
+    }
+};
+
+export default ControlButtons;
